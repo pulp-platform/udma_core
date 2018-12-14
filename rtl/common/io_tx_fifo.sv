eea5c93dc8a17526789683f20df9302eccc8be89
Igor Kobra Loi <igor.loi@greenwaves-technologies.com>
2018-12-14 10:33:19 +0100

    removed old log2 defines, replaced with clog2

--- rtl/common/io_tx_fifo.sv
+++ rtl/common/io_tx_fifo.sv
@@ -17,7 +17,6 @@
 // Authors    : Antonio Pullini (pullinia@iis.ee.ethz.ch)
 //
 ///////////////////////////////////////////////////////////////////////////////
-`define log2(VALUE) ((VALUE) < ( 1 ) ? 0 : (VALUE) < ( 2 ) ? 1 : (VALUE) < ( 4 ) ? 2 : (VALUE) < ( 8 ) ? 3 : (VALUE) < ( 16 )  ? 4 : (VALUE) < ( 32 )  ? 5 : (VALUE) < ( 64 )  ? 6 : (VALUE) < ( 128 ) ? 7 : (VALUE) < ( 256 ) ? 8 : (VALUE) < ( 512 ) ? 9 : (VALUE) < ( 1024 ) ? 10 : (VALUE) < ( 2048 ) ? 11 : (VALUE) < ( 4096 ) ? 12 : (VALUE) < ( 8192 ) ? 13 : (VALUE) < ( 16384 ) ? 14 : (VALUE) < ( 32768 ) ? 15 : (VALUE) < ( 65536 ) ? 16 : (VALUE) < ( 131072 ) ? 17 : (VALUE) < ( 262144 ) ? 18 : (VALUE) < ( 524288 ) ? 19 : (VALUE) < ( 1048576 ) ? 20 : (VALUE) < ( 1048576 * 2 ) ? 21 : (VALUE) < ( 1048576 * 4 ) ? 22 : (VALUE) < ( 1048576 * 8 ) ? 23 : (VALUE) < ( 1048576 * 16 ) ? 24 : 25)
 
 module io_tx_fifo
 #(
@@ -42,7 +41,7 @@ module io_tx_fifo
     output logic                    ready_o
 );
 
-    localparam LOG_BUFFER_DEPTH = `log2(BUFFER_DEPTH);
+    localparam LOG_BUFFER_DEPTH = $clog2(BUFFER_DEPTH);
 
     logic [LOG_BUFFER_DEPTH:0]      s_elements;    // number of elements in the buffer
     logic [LOG_BUFFER_DEPTH:0]      s_free_ele;    // number of free elements in the buffer
