3553b15b03f6c647cd1779e224bc15101ee0cd40
Alfio Di Mauro <adimauro@iis.ee.ethz.ch>
2021-08-11 17:32:08 +0200

    add typedef definition to expose peripheral plug in a convenient way

--- rtl/common/udma_pkg.sv
+++ rtl/common/udma_pkg.sv
@@ -55,5 +55,72 @@ package udma_pkg;
 	typedef struct packed {
 		logic         ready;
 	}udma_stream_rsp_t;
+
+	// cfg request
+	typedef struct packed {
+		logic [31:0] data;
+		logic [4:0] addr;
+		logic valid;
+		logic rwn;
+	} cfg_req_t;
+	// cfg response
+	typedef struct packed {
+		logic ready;
+		logic [31:0] data;
+	} cfg_rsp_t;
+
+	// tx channel
+	// req
+	typedef struct packed {
+		ch_transize_t bytes_left;
+		ch_addr_t curr_addr;
+		ch_data_t data;
+		logic en;
+		logic events;
+		logic gnt;
+		logic pending;
+		ch_stream_t stream;
+		ch_streamid_t stream_id;
+		logic valid;
+	}udma_linch_tx_req_t;
+	// rsp
+	typedef struct packed {
+		logic cen;
+		logic clr;
+		logic continuous;
+		ch_datasize_t datasize;
+		ch_dest_t destination;
+		logic ready;
+		logic req;
+		ch_transize_t size;
+		ch_addr_t startaddr;
+	}udma_linch_tx_rsp_t;
+
+	//  rx channel
+	// req
+	typedef struct packed {
+		logic cen;
+		logic clr;
+		logic continuous;
+		ch_data_t data;
+		ch_datasize_t datasize; 
+		ch_dest_t destination;
+		logic req;
+		ch_transize_t size;
+		ch_addr_t startaddr;
+		ch_stream_t stream;
+		ch_streamid_t stream_id;
+		logic valid; 
+	}udma_linch_rx_req_t;
+	// rsp
+	typedef struct packed {
+		ch_transize_t bytes_left;
+		ch_addr_t curr_addr;
+		logic en;
+		logic events;
+		logic gnt; 
+		logic pending; 
+		logic ready;
+	}udma_linch_rx_rsp_t;
 	
 endpackage
